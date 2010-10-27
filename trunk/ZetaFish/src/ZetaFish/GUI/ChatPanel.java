ac21c4931f54470033ef645718f6055a941d27e4
chad.e.albrecht@gmail.com <chad.e.albrecht@gmail.com@9332d4cb-b9c1-21f3-982e-104ac0623057>
2010-10-27 03:37:58 +0000

    wired up chatpanel to ZetaFishClient.

--- trunk/ZetaFish/src/ZetaFish/GUI/ChatPanel.java
+++ trunk/ZetaFish/src/ZetaFish/GUI/ChatPanel.java
@@ -3,6 +3,8 @@ package ZetaFish.GUI;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.event.*;
+
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -10,21 +12,31 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
+import ZetaFish.Interfaces.*;
+
 /**
  *
  * @author Patrick
  */
-public class ChatPanel extends Panel implements Runnable {
+public class ChatPanel extends Panel implements Runnable, IChatListener, ActionListener {
 
     private BoxLayout  chatLayout;
     private Dimension  dim;
     private JTextArea  outputText;
     private JTextField inputTest;
+    private INetworkManager nm;
+    
+    private JButton sendButton;
+    private final String SEND_ACTION = "send"; 
 
-    ChatPanel() {
+    ChatPanel(INetworkManager nm) {
         super(600,130);
+        
+        this.nm = nm;
 
-        JButton test = new JButton("Send");
+        sendButton = new JButton("Send");
+        sendButton.setActionCommand(SEND_ACTION);
+        sendButton.addActionListener(this);
 
         outputText = new JTextArea(6,2);
         inputTest  = new JTextField();
@@ -36,7 +48,7 @@ public class ChatPanel extends Panel implements Runnable {
         this.setLayout(new BorderLayout(3,3));
         this.add(new JScrollPane(outputText), BorderLayout.PAGE_START);
         this.add(inputTest, BorderLayout.CENTER);
-        this.add(test, BorderLayout.LINE_END);
+        this.add(sendButton, BorderLayout.LINE_END);
         this.setBorder(BorderFactory.createLineBorder(Color.black, 2));
     }
 
@@ -45,5 +57,36 @@ public class ChatPanel extends Panel implements Runnable {
     public void run() {
         //throw new UnsupportedOperationException("Not supported yet.");
     }
+
+    public void actionPerformed(ActionEvent e) 
+    {
+    	if(e.getActionCommand() == SEND_ACTION)
+        {
+    		try
+    		{
+    			this.nm.sendMessage(inputTest.getText());
+    			inputTest.setText("");
+    		}
+    		catch(Exception err)
+    		{    			
+    			HandleException(err);
+    		}
+        }	
+    }
+    
+    /**
+     * Single spot to determine how to handle exceptions
+     * @param err
+     */
+    private void HandleException(Exception err)
+    {
+    	err.printStackTrace();    	
+    }
+
+
+	@Override
+	public void OnNewMessage(String from, String msg) {
+		outputText.setText(outputText.getText() + from + ": " + msg + "\n");		
+	}
 
 }
