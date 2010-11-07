0faf99a56d1f2ce91ba9158993ae55b7cc79410f
patrickmichalina <patrickmichalina@9332d4cb-b9c1-21f3-982e-104ac0623057>
2010-11-07 16:52:45 +0000

    Fixed book spacing.

--- ZetaFish/src/ZetaFish/VersionInfo.java
+++ ZetaFish/src/ZetaFish/VersionInfo.java
@@ -3,7 +3,7 @@ package ZetaFish;
 public class VersionInfo {
 	public static final String revisionMajor="1";
 	public static final String revisionMinor="0";
-	public static final String revisionRevision="0";
+	public static final String revisionRevision="1";
 	
 	public static String version()
 	{
